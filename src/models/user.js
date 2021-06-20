d997ec8be92b9f992c41d32bc80b1301514afe4c
peter-keller <keller.f.peter@gmail.com>
2021-06-20 19:54:11 +0100

    feat: user model v1

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+import { DataTypes } from "sequelize"
+import { sequelize } from "../config";
+
+const User = sequelize.define("User", {
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default User
